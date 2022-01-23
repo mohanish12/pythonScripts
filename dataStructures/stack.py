59e6853208ee8bf85d23556ceefccef01fbf5cd8
Mohanish Mahajan <mohanish.mahajan12@gmail.com>
2022-01-23 16:16:53 +0530

    Add files via upload

---
+++ dataStructures/stack.py
@@ -0,0 +1,10 @@
+stack = []
+stack.append("a")
+stack.append("b")
+stack.append("c")
+
+print("Original Stack", stack)
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print("Stack in the end", stack)
